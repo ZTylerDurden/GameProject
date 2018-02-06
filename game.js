657debf371895cb89f74673446ff3e856a680f20
mosi.rivera <mosileko@yahoo.com>
2018-02-05 22:28:35 -0500

    Initial commit

--- game.js
+++ game.js
@@ -1,81 +1,75 @@
-window.onload = function() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var busterShot = new Image();
-    busterShot.src = "images/BassBuster.png";
-    var shootArray = [];
-   
-    
-    document.getElementById("start-button").onclick = function() {
-      startGame();
-      };
-    
+var canvas = document.getElementsByTagName('canvas')[0]
+var ctx = canvas.getContext("2d");
+var start = () => {
+  intervalId = setInterval(updateCanvas, 20);
+};
+var stop = () => {
+  clearInterval(intervalId);
+}
+var clear = () => {
+  ctx.clearRect(0,0,800,612);
+}
 
-      function startGame() {
-        draw(bshooter);
-        updateCanvas();
-      }
-    
-    
-        var bshooter = {
-          x: 400,
-          y: 470,
-          moveLeft:  function() { this.x -= 25 },
-          moveRight: function() { this.x += 25 },
-        }
-        
-         function draw(bshooter) {
-          var bsimg = new Image();
-          bsimg.onload = function() {
-            ctx.drawImage(bsimg, bshooter.x, bshooter.y, 49, 112);
-          }
-        bsimg.src = "images/ContraBlueMan.png";
-        }
-        
-        document.onkeydown = function(e) {
-          switch (e.keyCode) {
-            case 37: bshooter.moveLeft();  console.log('left',  bshooter); break;
-            case 39: bshooter.moveRight(); console.log('right', bshooter); break;
-            case 32: pushShooting(); console.log('spacebar'); break;
-          }
-          updateCanvas();
-        }
-        
-        function updateCanvas() {
-          ctx.clearRect(0,0,1411,1080);
-          draw(bshooter);
-          updateShooting();
-        }
-        
-        function Shooting() {
-            this.x = bshooter.x;
-            this.y = bshooter.y;
-            this.height = 10;
-            this.width = 10;
-            this.sprite = busterShot;
+var bsImg = new Image();
+bsImg.src = "images/ContraBlueMan.png";
 
-            var self = this;
-            this.draw = function() {
-                ctx.drawImage(busterShot, this.x + (bshooter.width/2 - this.width/2), this.y, this.width, this.height)
-                // ctx.fillStyle = "#FF0000";
-                // ctx.fillRect(this.x, this.y, self.width, self.height);
-            }
-            this.update = function() {
-                this.y -= 25
-            }
-        }       
-        
-        function updateShooting() {            
-            shootArray.forEach(function(elem) {
-                elem.update();
-                elem.draw();
-            })
-        }   
+var bBImage = new Image();
+bBImage.src = "images/BassBuster.png";
 
-        function pushShooting() {
-            shootArray.push(new Shooting());
-        }
-        updateCanvas();
+var shotsArr = [];
 
-    };
-    
+var bShooter = {
+  x:400,
+  y: 470,
+  image: bsImg,
+  moveLeft: function() {
+    this.x -= 25
+  },
+  moveRight: function() {
+    this.x += 25
+  },
+  draw: function() {
+    ctx.drawImage(this.image, this.x, this.y, 49, 112);
+  }
+
+}
+
+function Shot () {
+  this.x = bShooter.x;
+  this.y = bShooter.y;
+  this.height = 80;
+  this.width  = 80;
+  this.image = bBImage;
+  this.draw = function(){
+    var xpos = this.x + 49 / 2 - this.width / 2;
+    var ypos = this.y - this.height;
+    ctx.drawImage(this.image, xpos, ypos, this.width, this.height)  }
+  this.update = function() {
+    this.y -= 15;
+  }
+  console.log('const shot')
+}
+
+var pushShot = () => {
+  shotsArr.push(new Shot());
+  console.log('pushed shot')
+};
+
+
+
+updateCanvas = () => {
+  clear();
+  bShooter.draw();
+  shotsArr.forEach(elem => {
+    elem.draw();
+    elem.update();
+  })
+};
+
+window.addEventListener('keydown', (e) => {
+  switch (e.which) {
+    case 37: bShooter.moveLeft(); break;
+    case 39: bShooter.moveRight(); break;
+    case 32: pushShot(); break;
+  }
+})
